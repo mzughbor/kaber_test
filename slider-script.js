1048ec408952bcb5b37f0c87e38a5d9a0eeb37fc
zug <mzughbor@gmail.com>
2024-05-28 02:34:08 +0300

    latest test fixed

---
+++ slider-script.js
@@ -0,0 +1,24 @@
+
+const slideContainer = document.querySelector('.slide-container');
+const slides = document.querySelectorAll('.slide');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  slideContainer.style.transform = `translateX(-${index * 100}%)`;
+  currentSlide = index;
+}
+
+arrowLeft.addEventListener('click', () => {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+});
+
+arrowRight.addEventListener('click', () => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+});
+
+showSlide(currentSlide);
